bb3349d7aff9dea22832e8d69b77d682f5e76b0f
Michael Paulukonis <xraysmalevich@gmail.com>
2015-08-13 11:16:37 -0400

    change to the way ngrams are stored, random funcs, tests

--- js/markov.js
+++ js/markov.js
@@ -8,6 +8,28 @@ var Markov = function(order) {
     this.atoms = [];
     this.order = order || 2; // ergh
     this.ngrams = {};
+    this.keys = []; // will store the keys of ngram
+
+    this.pick = function(arr) {
+        return arr[Math.floor(Math.random()*arr.length)];
+    };
+
+    // NOTE: returns the value, not the key
+    // might want a function to return the kvp
+    // http://stackoverflow.com/a/15106541/41153
+    this.randomProperty = function () {
+        return this.ngrams[this.keys[this.keys.length * Math.random() << 0]];
+    };
+
+    // the generic-ness is all well-and-good
+    // but we will only ever be looking at the ngrams object
+    // and the keys can be pre-calculated
+    // so we don't have to do it more than once, nu?
+    // eh, wait on that...
+    this.randomNGram = function() {
+        var key = this.keys[this.keys.length * Math.random() << 0];
+        return { key: key, value: this.ngrams[key] };
+    };
 
     this.init = function(text) {
         this.originalText = text;
@@ -15,19 +37,34 @@ var Markov = function(order) {
         var ngrams = this.ngrams;
         var atoms = this.atoms;
 
-        // this is not right -- the order is always 1, here...
-        // plus, well, everything else
         for (var i = 0; i < atoms.length; i++) {
             var key = atoms.slice(i, i + this.order).join(' ');
             if (!ngrams[key]) {
-                ngrams[key] = [];
+                ngrams[key] = {
+                    key: atoms.slice(i, i + this.order),
+                    followers: []
+                };
+
             }
             if (i + this.order < atoms.length) {
-                ngrams[key].push(atoms[i + this.order]);
+                ngrams[key].followers.push(atoms[i + this.order]);
             }
         }
+        this.keys = Object.keys(ngrams);
         return true;
     };
+
+    this.generate = function(len) {
+        var words = [];
+
+        // TODO: implement
+        for (var i = 0; i < len; i ++) {
+
+        }
+
+        return words.join(' ');
+    };
+
 
     // start with the dumbest possible algorithm
     this.splitter = function(text) {
--- test/test_markov.js
+++ test/test_markov.js
@@ -3,72 +3,82 @@ var expect = chai.expect;
 
 describe("markov tests", function() {
 
-describe("api", function() {
+    describe("api", function() {
 
-    var m = new Markov();
+        var m = new Markov();
 
-    it("should have an init() method", function() {
-        expect(typeof(m.init)).to.equal('function');
+        it("should have an init() method", function() {
+            expect(typeof(m.init)).to.equal('function');
+        });
+
+        it("should have a splitter() method", function() {
+            expect(typeof(m.splitter)).to.equal('function');
+        });
+
+        it("should have a generate() method", function() {
+            expect(typeof(m.generate)).to.equal('function');
+        });
+
+        it("should have an 'originalText' property", function() {
+            expect(typeof(m.originalText)).to.equal('string');
+        });
+
+        it("should have an 'atoms' property", function() {
+            expect(typeof(m.atoms)).to.equal('object');
+        });
+
+        it("should have an 'ngrams' property", function() {
+            expect(typeof(m.ngrams)).to.equal('object');
+        });
+
+        it("should have an 'order' property", function() {
+            expect(typeof(m.order)).to.equal('number');
+        });
+
     });
 
-    it("should have a splitter() method", function() {
-        expect(typeof(m.splitter)).to.equal('function');
+
+    describe("initialization", function() {
+
+        var m = new Markov();
+        var source = "it will work or it will not work";
+
+        it("should not fail on init()", function() {
+            expect(m.init(source)).to.equal(true);
+        });
+
+        it("should expose the original text, unmodified", function() {
+            expect(m.originalText).to.equal(source);
+        });
+
+        it("should have a default order of 2", function() {
+            expect(m.order).to.equal(2);
+        });
+
+        it("passing a specific order should be retained", function() {
+            var order = 5;
+            var m = new Markov(order);
+            expect(m.order).to.equal(order);
+        });
+
     });
 
-    it("should have an 'originalText' property", function() {
-        expect(typeof(m.originalText)).to.equal('string');
+    describe("tokenization", function() {
+
+        // tokenization-level is hard-coded (no params)
+        var expected = [ "it", "will", "work", "or", "it", "will", "not", "work" ];
+        var source = "it will work or it will not work";
+        var m = new Markov();
+        m.init(source);
+
+        it("should turn '" + source + "' into 8 tokens", function() {
+            expect(m.atoms.length).to.equal(8);
+        });
+
     });
 
-    it("should have an 'atoms' property", function() {
-        expect(typeof(m.atoms)).to.equal('object');
+    describe("ngrams", function() {
+
     });
-
-    it("should have an 'ngrams' property", function() {
-        expect(typeof(m.ngrams)).to.equal('object');
-    });
-
-
-
-});
-
-
-describe("initialization", function() {
-
-    var m = new Markov();
-    var source = "it will work or it will not work";
-
-    it("should not fail on init()", function() {
-        expect(m.init(source)).to.equal(true);
-    });
-
-    it("should expose the original text, unmodified", function() {
-        expect(m.originalText).to.equal(source);
-    });
-
-    it("should have a default order of 2", function() {
-        expect(m.order).to.equal(2);
-    });
-
-    it("passing a specific order should be retained", function() {
-        var order = 5;
-        var m = new Markov(order);
-        expect(m.order).to.equal(order);
-    });
-
-});
-
-describe("tokenization", function() {
-
-    // tokenization-level is hard-coded (no params)
-    var expected = [ "it", "will", "work", "or", "it", "will", "not", "work" ];
-    var source = "it will work or it will not work";
-    var m = new Markov();
-    m.init(source);
-
-    it("should turn '" + source + "' into 8 tokens", function() {
-        expect(m.atoms.length).to.equal(8);
-    });
-
-});
 
 });
